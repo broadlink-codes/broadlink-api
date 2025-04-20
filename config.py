3c9f3014523b833f40d3df4a44da6cd36dc28ecf
Ishan Mishra <ishan2198@hotmail.com>
2025-04-20 21:11:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-__get_env = lambda key: config.get(key, os.environ.get(key))
+def __get_env(key, default=None):
+  return config.get(key, os.environ.get(key, default))
 
 ## TODO: add support for more devices
 DEVICE:Optional[broadlink.rm4mini] = None
@@ -22,8 +23,8 @@ def is_device_active(device: broadlink.rm4mini):
 DEVICE_NAME = __get_env("DEVICE_NAME")
 DEVICE_MAC = __get_env("DEVICE_MAC")
 DEVICE_IP = __get_env("DEVICE_IP")
-DEVICE_PORT = int(__get_env("DEVICE_PORT"))
-DEVICE_DEVTYPE = int(__get_env("DEVICE_DEVTYPE"))
+DEVICE_PORT = int(__get_env("DEVICE_PORT", 80))
+DEVICE_DEVTYPE = int(__get_env("DEVICE_DEVTYPE", 1122))
 DISCOVERY_TYPE = __get_env("DISCOVERY_TYPE")
 
 def get_device()-> broadlink.rm4mini:
